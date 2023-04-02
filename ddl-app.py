c53c755d1ddfc517f8c665528d880a2b0589cbc1
Julien SOFACK <sofa.kreutz^@yahoo.fr>
2023-04-02 20:28:35 +0200

    simplfy the code

--- ddl-app.py
+++ ddl-app.py
@@ -4,16 +4,17 @@ from largefunctions import ddl_app
 
 # path_data value if no personnal_data module
 path_data = None
-
 # Path where to download pics
 path = f"{personnal_data.path_data}"
-
-# Take the url and check the page status
-url, soup = get_the_url()
-# Data finisher
-type = check_category(soup, "2D", "3D")
-
+#
+def single_ddl(chosen_path):
+    # Take the url and check the page status
+    url, soup = get_the_url()
+    # Data finisher
+    type = check_category(soup, "2D", "3D")
+    # Start process
+    ddl_app(url, type, chosen_path)
 
 if __name__ == "__main__":
     # Start the process
-    ddl_app(url, type, path)
+    single_ddl(path)
--- file-reader-ddl.py
+++ file-reader-ddl.py
@@ -4,22 +4,22 @@ from largefunctions import ddl_app
 
 # path_data value if no personnal_data module
 path_data = None
-
 # Path where to download pics
 path = f"{personnal_data.path_data}"
-
-# Set to 0 for entering the loop
-n = 1
-# Set up the loop process
-while n != 0:
-    n = 0
-    for type in os.listdir(path):
-        if "txt" in type:
-            file = open(f"{path}/{type}", "r")
-            for line in file:
-                n += ddl_app(line.rstrip(), type, path)
-            file.close()
+#
+def check_txt_file(chosen_path):
+    # Set to 0 for entering the loop
+    n = 1
+    # Set up the loop process
+    while n != 0:
+        n = 0
+        for type in os.listdir(chosen_path):
+            if "txt" in type:
+                file = open(f"{chosen_path}/{type}", "r")
+                for line in file:
+                    n += ddl_app(line.rstrip(), type, chosen_path)
+                file.close()
 
 if __name__ == "__main__":
-    pass
-    # ddl_app(path)
+    # Start the process
+    check_txt_file(path)
