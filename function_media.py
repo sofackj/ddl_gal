1e00956dfaaf627387c743529fe3660122ad4198
Julien SOFACK <sofa.kreutz^@yahoo.fr>
2023-09-23 08:48:56 +0200

    add comment for unit in seconds

--- function_media.py
+++ function_media.py
@@ -3,7 +3,7 @@ from moviepy.editor import VideoFileClip, concatenate_videoclips
 def final_clip(clip_files_list, clips_widh, final_clip_destination, final_clip_name, limit_length=False, length_limit=0):
     # Create empty list to store clips
     _clip_list = []
-    # total_length
+    # total_length in seconds
     total_length = 0
     # Look for clip with the good extension
     # for clip in clip_files_list:
