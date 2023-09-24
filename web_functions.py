5f463ecfcc66666e1949a56f39e18f4bfa959146
Julien SOFACK <sofa.kreutz^@yahoo.fr>
2023-09-24 09:32:17 +0200

    add features for browser opening

---
+++ web_functions.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+# Open a windows <ith multiple tables
+# The path of the browser for windows should be exactly in the following format
+# "\"path/of/the/browser/app\""
+# Example ! "\"X:/Folder/Folder1/Folder2/brave.exe\""
+def browser_with_multi_tabs(browser_path,urls_list=["https://www.google.com"]):
+    for url in urls_list:
+        webbrowser.get(
+            f"{browser_path} %s --incognito"
+            ).open(url)
